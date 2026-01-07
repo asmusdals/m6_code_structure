8533a05c3a22aec5b5269b74b46f01afe952fe6a
Asmus Dalsgaard <weloveaus@gmail.com>
2026-01-07 09:49:59 +0100

    my commit message

--- src/cookie_cutter_demo/evaluate.py
+++ src/cookie_cutter_demo/evaluate.py
@@ -1,7 +1,7 @@
 import torch
 import typer
-from data import corrupt_mnist
-from model import MyAwesomeModel
+from .data import corrupt_mnist
+from .model import MyAwesomeModel
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
@@ -26,6 +26,8 @@ def evaluate(model_checkpoint: str) -> None:
         total += target.size(0)
     print(f"Test accuracy: {correct / total}")
 
+def main() -> None:
+    typer.run(evaluate)    
 
 if __name__ == "__main__":
     typer.run(evaluate)
